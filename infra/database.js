a26a8110d2e2866a2c79635656fcb227c88b2c2b
Gabriel Bastos Sandim <gabriel.bsandim@gmail.com>
2025-08-19 10:19:14 -0300

    refactor: added more logs to database errors

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,26 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  const dbEnvironment = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-
-  await client.connect();
+  };
+  const client = new Client(dbEnvironment);
 
   try {
+    await client.connect();
+
     const result = await client.query(queryObject);
+
     return result;
   } catch (error) {
     console.error(error);
+    console.log("DB ENV: ", dbEnvironment);
+
+    throw error;
   } finally {
     await client.end();
   }
